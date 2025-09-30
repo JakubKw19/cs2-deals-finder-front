df3c6529e531de17d0ddf415fdcc84ee0267cf20
JakubKw19 <kkwiecie@interia.pl>
2025-09-30 19:26:53 +0200

    configuration changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const projectConfig = {
-  nextauth_url: process.env.NEXTAUTH_URL ?? `http://100.122.79.62:3000`,
-  trpc_url: process.env.TRPC_SERVER_URL ?? "http://100.122.79.62:3000",
+  nextauth_url: `${process.env.NEXTAUTH_URL ?? `http://100.122.79.62`}:${process.env.FRONTEND_PORT}`,
+  trpc_url: `${process.env.TRPC_SERVER_URL ?? `http://100.122.79.62`}:${process.env.PORT}`,
 };
